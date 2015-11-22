40bda3917851010cb95a1d06d6f9ef0d70596481
lubenets_slava <vyacheslav.lubenets@maxymiser.com>
2015-11-22 10:55:20 +0200

    added fastclick

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,18 +4,18 @@ require.config({
         jquery: 'static/jQuery',
         underscore: 'static/underscore',
         backbone: 'static/backbone',
+        fastclick: 'static/fastclick',
         SingleMove: 'SingleMove',
         MultipleMoves: 'MultipleMoves',
         SingleMoveEdit: 'SingleMoveEdit'
     }
 });
 
-define(['jquery', 'underscore', 'backbone', 'SingleMove', 'MultipleMoves', 'SingleMoveEdit'],
-    function (jQuery, _, Backbone, SingleMove, MultipleMoves, SingleMoveEdit) {
+define(['jquery', 'underscore', 'backbone', 'SingleMove', 'MultipleMoves', 'SingleMoveEdit', 'fastclick'],
+    function (jQuery, _, Backbone, SingleMove, MultipleMoves, SingleMoveEdit, fastclick) {
 
         jQuery(function () {
+            fastclick.attach(document.body);
             new window.app.MultipleMovesCollectionView();
         });
     });
-
-
