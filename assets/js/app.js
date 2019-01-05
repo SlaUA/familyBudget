2bdf4f7279c92afe5b5dfff3efadeb7ffe83a469
Lubenets Slava <wenzelua@gmail.com>
2019-01-05 22:00:51 +0200

    add all years' savings chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,73 +1,73 @@
 require.config({
-		enforceDefine: true,
-		baseUrl      : 'assets/js/',
-		paths        : {
-				jquery                   : 'static/jQuery',
-				Dropbox                  : 'static/Dropbox',
-				datepicker               : 'static/datepicker',
-				configDatePicker         : 'static/configPicker',
-				underscore               : 'static/underscore',
-				backbone                 : 'static/backbone',
-				text                     : 'static/text',
-				backboneLocalStorage     : 'static/backboneLocalStorage',
-				fastclick                : 'static/fastclick',
-				highcharts               : 'static/highcharts',
-				MainRouter               : 'routers/MainRouter',
-				ViewsManager             : 'middleware/ViewsManager',
-				MultipleMovesCollection  : 'collections/MultipleMovesCollection',
-				SingleMoveModel          : 'models/SingleMoveModel',
-				SingleMoveView           : 'views/SingleMoveView',
-				MultipleMovesView        : 'views/MultipleMovesView',
-				SingleMoveEditView       : 'views/SingleMoveEditView',
-				SingleMoveAddView        : 'views/SingleMoveAddView',
-				MainMovesView            : 'views/MainMovesView',
-				MultipleSavingsCollection: 'collections/MultipleSavingsCollection',
-				SingleSavingModel        : 'models/SingleSavingModel',
-				MainSavingsView          : 'views/MainSavingsView',
-				SingleSavingEditView     : 'views/SingleSavingEditView',
-				SingleSavingAddView      : 'views/SingleSavingAddView',
-				SingleSavingView         : 'views/SingleSavingView',
-				MultipleSavingsView      : 'views/MultipleSavingsView',
-				WaitSpinnerView          : 'views/WaitSpinnerView',
-				MainMoveChartView        : 'views/MainMoveChartView',
-				MainSavingsChartView     : 'views/MainSavingsChartView',
-				MainSyncView             : 'views/MainSyncView'
-		},
-		shim         : {
-				highcharts: {
-						exports: 'Highcharts',
-						deps   : ['jquery']
-				}
+	enforceDefine: true,
+	baseUrl: 'assets/js/',
+	paths: {
+		jquery: 'static/jQuery',
+		Dropbox: 'static/Dropbox',
+		datepicker: 'static/datepicker',
+		configDatePicker: 'static/configPicker',
+		underscore: 'static/underscore',
+		backbone: 'static/backbone',
+		text: 'static/text',
+		backboneLocalStorage: 'static/backboneLocalStorage',
+		fastclick: 'static/fastclick',
+		highcharts: 'static/highcharts',
+		ramda: 'static/ramda',
+		MainRouter: 'routers/MainRouter',
+		ViewsManager: 'middleware/ViewsManager',
+		MultipleMovesCollection: 'collections/MultipleMovesCollection',
+		SingleMoveModel: 'models/SingleMoveModel',
+		SingleMoveView: 'views/SingleMoveView',
+		MultipleMovesView: 'views/MultipleMovesView',
+		SingleMoveEditView: 'views/SingleMoveEditView',
+		SingleMoveAddView: 'views/SingleMoveAddView',
+		MainMovesView: 'views/MainMovesView',
+		MultipleSavingsCollection: 'collections/MultipleSavingsCollection',
+		SingleSavingModel: 'models/SingleSavingModel',
+		MainSavingsView: 'views/MainSavingsView',
+		SingleSavingEditView: 'views/SingleSavingEditView',
+		SingleSavingAddView: 'views/SingleSavingAddView',
+		SingleSavingView: 'views/SingleSavingView',
+		MultipleSavingsView: 'views/MultipleSavingsView',
+		WaitSpinnerView: 'views/WaitSpinnerView',
+		MainMoveChartView: 'views/MainMoveChartView',
+		MainSavingsChartView: 'views/MainSavingsChartView',
+		MainSyncView: 'views/MainSyncView'
+	},
+	shim: {
+		highcharts: {
+			exports: 'Highcharts',
+			deps: ['jquery']
 		}
+	}
 });
 
 define([
-		'jquery',
-		'datepicker',
-		'configDatePicker',
-		'underscore',
-		'backbone',
-		'ViewsManager',
-		'fastclick',
-		'MainRouter'
+	'jquery',
+	'datepicker',
+	'configDatePicker',
+	'underscore',
+	'backbone',
+	'ViewsManager',
+	'fastclick',
+	'MainRouter'
 ], function (jQuery,
-             datepicker,
-             configDatePicker,
-             _,
-             Backbone,
-             ViewsManager,
-             fastclick,
-             MainRouter) {
-
+	datepicker,
+	configDatePicker,
+	_,
+	Backbone,
+	ViewsManager,
+	fastclick,
+	MainRouter) {
 		configDatePicker();
 
 		jQuery(function () {
 
-				window.app = {};
-				// remove click lag on mobile devices
-				fastclick.attach(document.body);
+			window.app = {};
+			// remove click lag on mobile devices
+			fastclick.attach(document.body);
 
-				new ViewsManager();
-				new MainRouter();
+			new ViewsManager();
+			new MainRouter();
 		});
-});
+	});
--- assets/js/views/MainSavingsChartView.js
+++ assets/js/views/MainSavingsChartView.js
@@ -1,124 +1,168 @@
-define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'highcharts',
-    'text!../templates/mainSavingsChartTemplate.html'
-], function (jQuery,
-             _,
-             Backbone,
-             Highcharts,
-             template) {
+define(['ramda', 'jquery', 'underscore', 'backbone', 'highcharts', 'text!../templates/mainSavingsChartTemplate.html'],
+    function (ramda, jQuery, _, Backbone, Highcharts, template) {
+        return Backbone.View.extend({
+            el: '#mainWrapper',
+            template: _.template(template),
+            $yearSelect: null,
+            currentYear: null,
+            collection: null,
+            events: {
+                'change #yearFilter': 'render',
+                'change .allYearsEnabled': 'render'
+            },
 
-    return Backbone.View.extend({
-
-        el         : '#mainWrapper',
-        template   : _.template(template),
-        $yearSelect: null,
-        currentYear: null,
-
-        collection: null,
-
-        events: {
-            'change #yearFilter': 'render'
-        },
-
-        initialize: function () {
-
-            if (!(window.app && window.app.multipleSavingsCollection)) {
-                return location.hash = '#savings';
-            }
-
-            this.$el.append(
-                this.template({
-                    currentYear: new Date().getFullYear()
-                })
-            );
-            this.collection  = window.app.multipleSavingsCollection;
-            this.$yearSelect = this.$el.find('#yearFilter');
-            this.render.call(this);
-            window.app.trigger('addNewView', this);
-        },
-
-        /**
-         * @returns {Array}, array of savings for the selected year
-         */
-        getSeries: function () {
-            var yearSavings = {
-                expense: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                income: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            };
-
-            this.currentYear = parseInt(this.$yearSelect.val());
-
-            this.collection.each((saving) => {
-                const dateOfSaving = new Date(saving.attributes.date);
-
-                if (dateOfSaving.getFullYear() !== this.currentYear || saving.attributes.currency !== 'USD') {
-                    return false;
+            initialize: function () {
+                if (!(window.app && window.app.multipleSavingsCollection)) {
+                    return location.hash = '#savings';
                 }
 
-                if (saving.attributes.type === 'expense') {
-                    yearSavings[saving.attributes.type][dateOfSaving.getMonth()] -= saving.attributes.sum;
-                } else {
-                    yearSavings[saving.attributes.type][dateOfSaving.getMonth()] += saving.attributes.sum;
-                }
-            });
+                this.$el.append(
+                    this.template({
+                        currentYear: new Date().getFullYear()
+                    })
+                );
+                this.collection = window.app.multipleSavingsCollection;
+                this.$yearSelect = this.$el.find('#yearFilter');
+                this.render.call(this);
+                window.app.trigger('addNewView', this);
+            },
 
-            return [{
-                name: 'Доход',
-                data: yearSavings.income
-            }, {
-                name: 'Затраты',
-                data: yearSavings.expense
-            }];
-        },
+            /**
+             * @returns {Array}, array of savings for the selected year
+             */
+            getSingleYearSeries: function () {
+                var yearSavings = {
+                    expense: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    income: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                };
 
-        render: function () {
-            this.$el.find('#savingsChart')
-                .highcharts({
-                    chart : {
-                        type: 'spline'
-                    },
-                    plotOptions: {
-                        spline: {
-                            marker: {
-                                enabled: true
-                            },
-                            dataLabels: {
-                                enabled: true
-                            },
-                            enableMouseTracking: false
-                        }
-                    },
-                    title : {
-                        text: 'Сбережения'
-                    },
-                    xAxis : {
-                        categories: [
-                            'Январь',
-                            'Февраль',
-                            'Март',
-                            'Апрель',
-                            'Май',
-                            'Июнь',
-                            'Июль',
-                            'Август',
-                            'Сентябрь',
-                            'Октябрь',
-                            'Ноябрь',
-                            'Декабрь'
-                        ]
-                    },
-                    yAxis : {
-                        title: {
-                            text: 'сумма'
-                        }
-                    },
-                    series: this.getSeries.call(this)
+                this.currentYear = parseInt(this.$yearSelect.val());
+
+                this.collection.each((saving) => {
+                    const dateOfSaving = new Date(saving.attributes.date);
+
+                    if (dateOfSaving.getFullYear() !== this.currentYear || saving.attributes.currency !== 'USD') {
+                        return false;
+                    }
+
+                    if (saving.attributes.type === 'expense') {
+                        yearSavings[saving.attributes.type][dateOfSaving.getMonth()] -= saving.attributes.sum;
+                    } else {
+                        yearSavings[saving.attributes.type][dateOfSaving.getMonth()] += saving.attributes.sum;
+                    }
                 });
 
-            window.app.trigger('pageChangeEnd');
-        }
+                return [{
+                    name: 'Доход',
+                    data: yearSavings.income
+                }, {
+                    name: 'Затраты',
+                    data: yearSavings.expense
+                }];
+            },
+
+            groupModelsByYear: ramda.groupBy((model) => new Date(model.get('date')).getFullYear()),
+
+            getFullSeries: function () {
+                const groupedData = this.groupModelsByYear(this.collection.models);
+
+                return Object.entries(groupedData)
+                    .reduce(([incomeData, expenseData], [year, models]) => {
+                        let incomes = 0;
+                        let expenses = 0;
+
+                        models.forEach((saving) => {
+                            const type = saving.get('type');
+                            const currency = saving.get('currency');
+
+                            if (currency !== 'USD') {
+                                return;
+                            }
+
+                            if (type === 'expense') {
+                                expenses -= saving.attributes.sum;
+                            } else {
+                                incomes += saving.attributes.sum;
+                            }
+                        });
+
+                        return [{
+                            ...incomeData,
+                            data: [...incomeData.data, incomes]
+                        }, {
+                            ...expenseData,
+                            data: [...expenseData.data, expenses]
+                        }];
+                    }, [{
+                        name: 'Доход',
+                        data: []
+                    }, {
+                        name: 'Затраты',
+                        data: []
+                    }]);
+            },
+
+            getAllYearsCategories: function () {
+                return Object.keys(
+                    this.groupModelsByYear(this.collection.models)
+                );
+            },
+
+            render: function () {
+                const isAllYearsEnabled = this.$el.find('.allYearsEnabled').prop('checked');
+                const yearSelectStuff = this.$yearSelect.closest('.dateFilter').find('.yearSelectWrapper, .yearSelectLabel');
+
+                if (isAllYearsEnabled) {
+                    yearSelectStuff.hide();
+                } else {
+                    yearSelectStuff.show();
+                }
+
+                this.$el
+                    .find('#savingsChart')
+                    .highcharts({
+                        chart: {
+                            type: 'spline'
+                        },
+                        plotOptions: {
+                            spline: {
+                                marker: {
+                                    enabled: true
+                                },
+                                dataLabels: {
+                                    enabled: true
+                                },
+                                enableMouseTracking: false
+                            }
+                        },
+                        title: {
+                            text: 'Сбережения'
+                        },
+                        xAxis: {
+                            categories: isAllYearsEnabled ? this.getAllYearsCategories.call(this) : [
+                                'Январь',
+                                'Февраль',
+                                'Март',
+                                'Апрель',
+                                'Май',
+                                'Июнь',
+                                'Июль',
+                                'Август',
+                                'Сентябрь',
+                                'Октябрь',
+                                'Ноябрь',
+                                'Декабрь'
+                            ]
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'сумма'
+                            }
+                        },
+                        series: isAllYearsEnabled ? this.getFullSeries.call(this) : this.getSingleYearSeries.call(this),
+                    });
+
+                window.app.trigger('pageChangeEnd');
+            }
+        });
     });
-});
